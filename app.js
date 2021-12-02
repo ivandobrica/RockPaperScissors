500a54ac9fde66bfa9025e43a5f190824aedd64d
ivandobrica <dobrica.ivan@hotmail.com>
2021-12-02 01:45:29 +0100

    Start game/remove intro

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const intro = document.querySelector(".intro");
-const playButton = document.querySelector(".intro__button");
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
 
-playButton.addEventListener("click", () => {
-  intro.classList.add("fade");
-  intro.addEventListener("transitionend", () => {
-    intro.style.display = "none";
-  });
-});
+  const startGame = () => {
+    const intro = document.querySelector(".intro");
+    const playButton = document.querySelector(".intro__button");
+    const match = document.querySelector(".match");
+
+    playButton.addEventListener("click", () => {
+      intro.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+      intro.addEventListener("transitionend", () => {
+        intro.style.display = "none";
+      });
+    });
+  };
+
+  startGame();
+};
+
+game();
