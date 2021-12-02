089fad96a23849669a42031a4cfd5eb85fff5cf9
ivandobrica <dobrica.ivan@hotmail.com>
2021-12-02 02:12:16 +0100

    Refactored code

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ const game = () => {
         setTimeout(() => {
           compareHands(this.textContent, computerChoice);
 
-          playerHand.src = `./${this.textContent}.png`;
-          computerHand.src = `./${computerChoice}.png`;
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
         }, 2000);
 
         playerHand.style.animation = "shakePlayer 2s ease";
