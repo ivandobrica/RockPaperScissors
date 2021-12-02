052b463e3a7c71369231456571b3f6d19781009e
ivandobrica <dobrica.ivan@hotmail.com>
2021-12-02 01:50:22 +0100

    Compare results, update score

--- app.js
+++ app.js
@@ -28,10 +28,72 @@ const game = () => {
         const randomNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[randomNumber];
 
+        compareHands(this.textContent, computerChoice);
+
         playerHand.src = `./${this.textContent}.png`;
         computerHand.src = `./${computerChoice}.png`;
       });
     });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector(
+      ".player-score .score__subtitle"
+    );
+    const computerScore = document.querySelector(
+      ".computer-score .score__subtitle"
+    );
+
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    const winner = document.querySelector(".winner");
+
+    if (playerChoice === computerChoice) {
+      winner.textContent = "Draw";
+      return;
+    }
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player wins";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer wins";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
   };
 
   startGame();
